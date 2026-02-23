82d5a7f98141acc2a49fd3f1f0e88ed2840f6787
daibing <daibing@bilibili.com>
2026-02-23 10:06:04 +0800

    feat: 更新启动脚本，添加云游戏和无头浏览器配置

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 set -e
 
-if [ "$(uname -m)" = "aarch64" ]; then
-  export MARCH7TH_BROWSER_TYPE=chromium
-  export MARCH7TH_BROWSER_PATH=/usr/bin/chromium
-  export MARCH7TH_DRIVER_PATH=/usr/bin/chromedriver
-fi
+export MARCH7TH_CLOUD_GAME_ENABLE=true
+export MARCH7TH_BROWSER_HEADLESS_ENABLE=true
+export MARCH7TH_BROWSER_HEADLESS_RESTART_ON_NOT_LOGGED_IN=false
+# 默认使用官方源下载浏览器；如果下载缓慢，可以改为 true 启用镜像下载
+export MARCH7TH_BROWSER_DOWNLOAD_USE_MIRROR=true
+# 任务完成后循环执行
+export MARCH7TH_AFTER_FINISH=Loop
+# 标记从 Docker 启动，避免控制台阻塞
+export MARCH7TH_DOCKER_STARTED=true
 
-exec "$@"
+
+export PYTHONDONTWRITEBYTECODE=1
+export PYTHONUNBUFFERED=1
+
+
+python main.py 
